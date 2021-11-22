41d2ce1898ecfc7dc7cb7adfce1796b30d376676
佳 <93702950+fe6ruar2y@users.noreply.github.com>
2021-11-22 17:49:45 +0900

    1978. 소수 찾기

--- Silver/1978.cpp
+++ Silver/1978.cpp
@@ -5,7 +5,7 @@ int main(){
     int n = 0; // 입력할 갯수 선언
     int a = 0; // 입력할 갯수 받기
     int count = 0;
-    int result = 0;
+    int result = 0; // 결과
     cin >> n;
 
     for(int i=0; i<n; i++){
