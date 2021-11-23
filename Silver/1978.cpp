e5d1af38813444922af653a3a5af63573a0ecfea
佳 <93702950+fe6ruar2y@users.noreply.github.com>
2021-11-23 19:09:33 +0900

    1978. 소수찾기
    count랑 result의 연결고리를 찾아냄,, 휴

--- Silver/1978.cpp
+++ Silver/1978.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int n = 0; // 입력할 갯수 선언
     int a = 0; // 입력할 갯수 받기
-    int count = 0;
+    int count = 0; // 나머지 갯수
     int result = 0; // 결과
     cin >> n;
 
@@ -18,7 +18,7 @@ int main(){
         }
         if(count == 2){
             result++;
-        }
+        } // 나머지 갯수가 2개면 소수니까 result에 추가
         count = 0;
     }
     cout << result << '\n';
