5dee0234423dd507eb59e75af17b5562445a9f49
fe6ruar2y <89909368+fe6ruar2y@users.noreply.github.com>
2021-12-15 18:39:55 +0900

    2751. 수 정렬하기

---
+++ 정렬/2751.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int n; // 테스트 케이스
+    int number[1000001] = {};
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> number[i];
+    }
+
+    sort(number, number+n);
+    for(int i=0; i<n; i++){
+        cout << number[i] << '\n';
+    }
+    return 0;
+}
