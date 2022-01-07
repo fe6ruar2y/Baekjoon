a3e64ad181ff062d27671b0168385100c5366c8c
fe6ruar2y <89909368+fe6ruar2y@users.noreply.github.com>
2022-01-07 18:31:31 +0900

    9020. 골드바흐의 추측

---
+++ Silver/9020.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool prime(int i){
+    int result;
+    result = sqrt(i);
+    if(result == 1 && i != 1){
+        return true;
+    } // 2,3 일 경우
+    if(i%2){
+        for(int j=2; j<= result; j++){
+            if(!(i%j)){
+                return false;
+            }
+            if(j == result){
+                return true;
+            }
+        } // 홀수일 경우
+    }
+} // 소수인지 아닌지 판별하는 함수 선언
+
+int main(){
+    int t; //테스트 케이스
+    cin >> t;
+
+    for(int i=0; i<t; i++){
+        int number;
+        cin >> number;
+
+        for(int j= number/2; j>=2; j--){
+            if(prime(j) && prime(number - j)){
+                cout << j << " " << number - j << '\n';
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
