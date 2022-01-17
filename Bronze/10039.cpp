dec76e9e145b6c9d0fc189abc5b2ff3055cd155a
佳 <93702950+fe6ruar2y@users.noreply.github.com>
2022-01-17 19:13:57 +0900

    10039. 평균 점수

--- Bronze/10039.cpp
+++ Bronze/10039.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
-#define student 5; // 학생 수
-#define pass 40; // 점수 제한
+#define STUDENT 5 // 학생 수
+#define PASS 40 // 점수 제한
 using namespace std;
+
+int main(){
+    int score[STUDENT];
+    int sum = 0; // 총합
+    int average = 0; // 평균
+
+    for(int i =0; i<STUDENT; i++){
+        cin >> score[i];
+    } // 점수 입력
+
+    for(int i=0; i<STUDENT; i++){
+        if(score[i]<PASS){
+            score[i] = 40;
+        } // 40점 미만이면 40점 출력
+        sum += score[i];
+    }
+    average = sum / STUDENT;
+    cout << average << '\n';
+    return 0;
+}
