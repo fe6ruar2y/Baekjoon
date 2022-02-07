ddb2b12a5b67d3010d109e3e07f9b871d14d3e1e
fe6ruar2y <rkdudrla26@naver.com>
2022-02-07 18:23:11 +0900

    3046. R2

---
+++ Bronze/3046.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int r1, average; // r1과 평균
+    int r2 = 0;
+
+    cin >> r1 >> average;
+    r2 = 2 * average - r1;
+
+    cout << r2 << '\n';
+    return 0;
+}
