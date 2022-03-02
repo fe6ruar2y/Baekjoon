608e1abf8a285c81176bce6887298d8bf34cf29c
fe6ruar2y <rkdudrla26@naver.com>
2022-03-02 18:21:38 +0900

    2558. A+B-2

---
+++ Bronze/2558.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a;
+    cin >> b;
+    
+    int result = 0;
+    result = a+b;
+    cout << result << '\n';
+    return 0;
+}
